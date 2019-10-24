7b4b95ebff41f051f62d37dc82c0ef410c0b2e5a
ReginaldoMSJr <reginaldo.silva22@hotmail.com>
2019-10-24 16:07:01 -0300

    Finalizando

--- Escola/Coordenador.cs
+++ Escola/Coordenador.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 
-namespace Projeto
-{
-    class Coordenador : Pessoa
-    {
+namespace Projeto {
+    class Coordenador : Pessoa {
         public int Registro;
 
-        public void CadastrarCoordenador(Escola escola)
-        {
+        public void CadastrarCoordenador(Escola escola) {
             Console.ForegroundColor = ConsoleColor.Yellow;
             Console.WriteLine("=================== Cadastro Coordenador ==================\n");
             Console.ResetColor();
@@ -16,22 +13,19 @@ namespace Projeto
             while (escola.coordenadores.Exists(x => x.Registro == Registro));
             Console.WriteLine($"Numero de registro: {Registro}");
             //Pergunta o nome do coordenador, e faz a validação na classe pessoa
-            do
-            {
+            do {
                 Console.Write("Digite o nome do coordenador: ");
                 Nome = Console.ReadLine();
             }
             while (Nome == null);
             //Pergunta a idade do coordenador, e faz a validação na classe pessoa
-            do
-            {
+            do {
                 Console.Write($"Digite a idade do(a) {Nome} : ");
                 Idade = (Console.ReadLine());
             }
             while (Idade == "");
             //Pergunta o sexo do coordenador, e faz a validação na classe pessoa
-            do
-            {
+            do {
                 Console.Write($"Qual o sexo do(a) {Nome} (M / F): ");
                 Sexo = (Console.ReadLine());
             }
